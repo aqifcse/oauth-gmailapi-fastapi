7b9214ee0884c345a2bb863869514baacb7202e6
user <aqif.cse@gmail.com>
2023-12-09 17:51:38 +0600

    Technical difficulty mail added

--- app/main.py
+++ app/main.py
@@ -42,43 +42,44 @@ app.add_middleware(
 )
 
 def send_email_background(user: User):
-  if user:  
-    emailMsg = 'Welcome!!' + user.full_name
+  emailMsg = 'Welcome!!'
+  mimeMessage = MIMEMultipart()
+  mimeMessage['to'] = user.email
+  mimeMessage['subject'] = 'Your account has been created!!!'
+  mimeMessage.attach(MIMEText(emailMsg, 'plain'))
+  raw_string = base64.urlsafe_b64encode(mimeMessage.as_bytes()).decode()
+
+  try:
+    # Send Mail
+    service= create_service_with_client_secret(CLIENT_SECRET_FILE, APPLICATION_NAME, API_NAME, API_VERSION, SCOPES)
+    
+    # For Service account 
+    # service = create_service_with_service_account(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
+
+    return service.users().messages().send(userId='me', body={'raw': raw_string}).execute()
+
+  except Exception as e:
+    raise HTTPException(status_code=400, detail=str(e))
+  
+def send_email_background_during_technical_difficulty(user):
+  try:
+    # Send Mail
+    service= create_service_with_client_secret(CLIENT_SECRET_FILE, APPLICATION_NAME, API_NAME, API_VERSION, SCOPES)
+    
+    # For Service account 
+    # service = create_service_with_service_account(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
+
+    emailMsg = "Sorry, Due to some technical difficulty your registration was not successful. Please re-register"
     mimeMessage = MIMEMultipart()
     mimeMessage['to'] = user.email
-    mimeMessage['subject'] = 'Your account has been created!!!'
+    mimeMessage['subject'] = 'Your registration is not successful'
     mimeMessage.attach(MIMEText(emailMsg, 'plain'))
     raw_string = base64.urlsafe_b64encode(mimeMessage.as_bytes()).decode()
 
-    try:
-      # Send Mail
-      service= create_service_with_client_secret(CLIENT_SECRET_FILE, APPLICATION_NAME, API_NAME, API_VERSION, SCOPES)
-      
-      # For Service account 
-      # service = create_service_with_service_account(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
+    return service.users().messages().send(userId='me', body={'raw': raw_string}).execute()
+  except Exception as e:
+    raise HTTPException(status_code=400, detail=str(e))
 
-      return service.users().messages().send(userId='me', body={'raw': raw_string}).execute()
-
-    except Exception as e:
-      raise HTTPException(status_code=400, detail=str(e))
-  else:
-    try:
-      # Send Mail
-      service= create_service_with_client_secret(CLIENT_SECRET_FILE, APPLICATION_NAME, API_NAME, API_VERSION, SCOPES)
-      
-      # For Service account 
-      # service = create_service_with_service_account(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
-
-      emailMsg = 'Sorry ' + user.full_name + ", Due to some technical difficulty your registration was not successful. Please re-register"
-      mimeMessage = MIMEMultipart()
-      mimeMessage['to'] = user.email
-      mimeMessage['subject'] = 'Your registration is not successful'
-      mimeMessage.attach(MIMEText(emailMsg, 'plain'))
-      raw_string = base64.urlsafe_b64encode(mimeMessage.as_bytes()).decode()
-
-      return service.users().messages().send(userId='me', body={'raw': raw_string}).execute()
-    except Exception as e:
-      raise HTTPException(status_code=400, detail=str(e))
 
 
     
@@ -89,7 +90,7 @@ def database_user_create(user: User):
  
   # creating user in the database
   hashed_password = bcrypt.hashpw(user.password.encode('utf-8'), bcrypt.gensalt())
-  user = ModelUser(username=user.username, email=user.email, password=hashed_password, full_name=user.full_name, disabled=False)
+  user = ModelUser(username=user.username, email=user.email, password=hashed_password, full_name=user.full_name, disabled=user.disabled)
   db.add(user)
   db.commit()
   db.refresh(user)
@@ -97,25 +98,29 @@ def database_user_create(user: User):
   if user.id:
     return user
   else:
-    raise HTTPException(status_code=500, detail="Failed to create user")
+    return False
    
    
 
 @app.post("/register")
 async def create_user(user: User, background_tasks: BackgroundTasks):
 
-  # creating user in the database
-  stored_user = database_user_create(user)
-
-  # sending email as a background task
   try:
+    # creating user in the database
+    stored_user = database_user_create(user)
+    # sending email as a background task
     background_tasks.add_task(
       send_email_background, stored_user
     )
+
+    expired_date = convert_iso_to_dhaka_time()
+
+    response = { "message":"Success!! Successfully registered. Welcome email sent. Email sending token will be expired at " + expired_date }
+    return response
   except Exception as e:
-    raise HTTPException(status_code=400, detail=str(e))
+    background_tasks.add_task(
+      send_email_background_during_technical_difficulty, user
+    )
+    return HTTPException(status_code=400, detail=str(e))
+    
 
-  expired_date = convert_iso_to_dhaka_time()
-
-  response = { "message":"Success!! Successfully registered. Welocme email sent. Email sending token will be expired at " + expired_date }
-  return response
--- app/models.py
+++ app/models.py
@@ -5,8 +5,8 @@ from .db import Base
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String(150))
-    email = Column(String(150))
-    password = Column(String(150))
-    full_name = Column(String(150))
-    disabled = Column(Boolean)# Create tables based on the models
+    username = Column(String(150), nullable=False)
+    email = Column(String(150), nullable=False)
+    password = Column(String(150), nullable=False)
+    full_name = Column(String(150), nullable=False)
+    disabled = Column(Boolean, nullable=False)# Create tables based on the models
